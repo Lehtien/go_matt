6764f6abf3a87b2e8b36d6d50f257b6981537d78
Lehti <leaf.mmo@gmail.com>
2023-09-22 04:35:03 +0900

    first commit

--- app/main.go
+++ app/main.go
@@ -11,5 +11,4 @@ func main () {
         return c.String(http.StatusOK, "Hello, World!")
      })
      e.Logger.Fatal(e.Start(":8082"))
-     // e.Startの中はdocker-composeのgoコンテナで設定したportsを指定してください。
 }
